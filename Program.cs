69c54eab66d60c3559f89d254b9c2ebbd82662e8
manunasci <nascimentomanuella73@gmail.com>
2021-08-25 19:28:16 -0300

    Apresenta o programa

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Boxs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("+----------+");
+            Console.WriteLine("| Manuella |");
+            Console.WriteLine("+----------+");
+        }
+    }
+}
